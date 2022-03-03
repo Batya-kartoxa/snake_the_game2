0d170138644f5614eed361ed7eec671db57ef39f
Batya-kartoxa <100840170+Batya-kartoxa@users.noreply.github.com>
2022-03-03 21:30:58 +0700

    Add files via upload

---
+++ snake.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define snake_size[5400]
+int snake (int *snake_size, int *snake_body, int snake_speed, int *head, int *damage)
+int fruit ()
